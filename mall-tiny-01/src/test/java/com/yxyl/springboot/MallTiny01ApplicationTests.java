347728192881ae5b950039b42bb206050ca19de8
YxYL6125 <1960378531@qq.com>
2022-04-19 22:25:33 +0800

    初始化架构
    分包
    yml配置文件的初始化
    pom依赖的添加

---
+++ mall-tiny-01/src/test/java/com/yxyl/springboot/MallTiny01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxyl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTiny01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
